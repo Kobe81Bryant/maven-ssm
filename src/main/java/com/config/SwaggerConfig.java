c7a34f63bc139a777eabd9f0f515d160e443c4bc
Kobe <971054162@qq.com>
2018-09-17 21:28:02 +0800

    首次提交

---
+++ src/main/java/com/config/SwaggerConfig.java
@@ -0,0 +1,45 @@
+package com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfoHandlerMapping;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket userApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
+                .select()  // 选择那些路径和api会生成document
+                    //.apis(RequestHandlerSelectors.any()) // 对所有api进行监控
+                    .apis(RequestHandlerSelectors.basePackage("com.controller"))
+                    .paths(PathSelectors.any()) // 对所有路径进行监
+                .build();
+    }
+
+    private ApiInfo getApiInfo(){
+        ApiInfo apiInfo = new ApiInfoBuilder()
+                .title("Kobe项目模板")
+                .description("描述信息")
+                .version("1.1")
+                .build();
+        return apiInfo;
+    }
+
+    @Bean
+    public RequestMappingInfoHandlerMapping requestMapping(){
+        return new RequestMappingHandlerMapping();
+    }
+}
---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,40 @@
+package com.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping("test")
+@Api(description="asd测试")
+public class TestController {
+    @SuppressWarnings("rawtypes")
+	@GetMapping("/test")
+    @ResponseBody
+    @ApiOperation(value = "swarger第一个测试方法")
+    public Map test1(){
+        Map<String,Object>map=new HashMap<>();
+        map.put("a",1);
+//        Thread thread=new Thread(new Runnable() {
+//			@Override
+//			public void run() {
+//				for (int i = 0; i < 2147483647; i++) {
+//					System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
+//					
+//				}
+//			}
+//		});
+//        thread.start();
+        for (int i = 0; i < 500000; i++) {
+			System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
+			
+		}
+        return map;
+    }
+}
