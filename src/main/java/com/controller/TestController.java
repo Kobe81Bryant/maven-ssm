e20e62b087d7716535f55543c9781585986ecf62
KobeBryant <971054162@qq.com>
2018-09-18 17:35:06 +0800

    添加忽略

--- src/main/java/com/controller/TestController.java
+++ src/main/java/com/controller/TestController.java
@@ -14,27 +14,26 @@ import java.util.Map;
 @RequestMapping("test")
 @Api(description="asd测试")
 public class TestController {
-    @SuppressWarnings("rawtypes")
 	@GetMapping("/test")
     @ResponseBody
     @ApiOperation(value = "swarger第一个测试方法")
     public Map test1(){
         Map<String,Object>map=new HashMap<>();
         map.put("a",1);
-//        Thread thread=new Thread(new Runnable() {
-//			@Override
-//			public void run() {
-//				for (int i = 0; i < 2147483647; i++) {
-//					System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
-//					
-//				}
-//			}
-//		});
-//        thread.start();
-        for (int i = 0; i < 500000; i++) {
-			System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
-			
-		}
+        Thread thread=new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for (int i = 0; i < 2147483647; i++) {
+					System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
+
+				}
+			}
+		});
+        thread.start();
+//        for (int i = 0; i < 2147483647; i++) {
+//			System.out.println("线程名称"+Thread.currentThread().getName()+"标识"+i);
+//
+//		}
         return map;
     }
 }
