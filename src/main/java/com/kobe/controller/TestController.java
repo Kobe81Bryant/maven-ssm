5346a266b314e035e2477737240f2c48adcde0c0
Kobe <971054162@qq.com>
2018-10-13 13:26:45 +0800

    111

--- src/main/java/com/kobe/controller/TestController.java
+++ src/main/java/com/kobe/controller/TestController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @RequestMapping("test")
 @Api(description="asd测试")
 public class TestController {
+	@SuppressWarnings("rawtypes")
 	@GetMapping("/test")
     @ResponseBody
     @ApiOperation(value = "swarger第一个测试方法")
