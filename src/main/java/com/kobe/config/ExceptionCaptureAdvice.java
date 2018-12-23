9ccb8f0f8341959c5bbe429fc50a433aa359a30d
Kobe <971054162@qq.com>
2018-12-24 00:29:40 +0800

    添加异常捕获

---
+++ src/main/java/com/kobe/config/ExceptionCaptureAdvice.java
@@ -0,0 +1,25 @@
+package com.kobe.config;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionCaptureAdvice {
+    /**
+     * 全局异常捕捉处理
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public Map errorHandler(Exception ex) {
+        Map map = new HashMap();
+        map.put("code", 100);
+        map.put("msg", ex.getMessage());
+        return map;
+    }
+}
