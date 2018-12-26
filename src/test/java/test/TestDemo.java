6db3d866ca539dbca020ee4e98385f9b0586cfe9
liuqiang147 <liuqiang147@jd.com>
2018-12-26 12:42:32 +0800

    上传

--- src/test/java/test/TestDemo.java
+++ src/test/java/test/TestDemo.java
@@ -1,10 +1,15 @@
 package test;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import javax.swing.*;
+
+//@RunWith(SpringJUnit4ClassRunner.class)
 public class TestDemo {
 	@Test
 	public void test1() {
-		
+		System.out.println(123);
 	}
 }
