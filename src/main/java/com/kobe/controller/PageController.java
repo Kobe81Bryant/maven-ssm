203f1e044d39af257767fc4db7502bb1acd753ac
liuqiang147 <liuqiang147@jd.com>
2019-02-21 17:38:32 +0800

    asdad

--- src/main/java/com/kobe/controller/PageController.java
+++ src/main/java/com/kobe/controller/PageController.java
@@ -22,7 +22,7 @@ public class PageController {
 		map.put("appname","jiashu");
 		map.put("type","小程序");
 		map.put("sss","aaa");
-		map.put("aaa","sss");
+		map.put("aaa","ssss");
 		return map;
 	}
 
