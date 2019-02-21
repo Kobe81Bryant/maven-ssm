26b7803bb44a0e0b5ac28eabe9a4ba1ca9be6eeb
Kobe <971054162@qq.com>
2019-02-21 22:47:06 +0800

    添加异常捕获

--- src/test/java/test/TestDemo.java
+++ src/test/java/test/TestDemo.java
@@ -1,11 +1,22 @@
 package test;
 
 import com.kobe.entity.Orders;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import javax.swing.*;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -17,24 +28,7 @@ import java.util.stream.Collectors;
 //@RunWith(SpringJUnit4ClassRunner.class)
 public class TestDemo {
     @Test
-    public void test1() {
-//        Map<String, Map<String, Map<Integer, List<BalanceOrders>>>> unionMaps =
-// balanceOrders.stream().collect(Collectors.groupingBy(BalanceOrders::getCommissionPin,
-//                Collectors.groupingBy(BalanceOrders::getFinishDate, Collectors.groupingBy(BalanceOrders::getOrderTypeJingTiao))));
-        List<Orders> list = new ArrayList<>();
-        Map<Date, List<Orders>> collect = list.stream().collect(Collectors.groupingBy(Orders::getPayDatetime));
-        Map<Date, List<Orders>> collect1 = list.stream()
-                .collect(Collectors.groupingBy(Orders::getPayDatetime));
-
-        Long a = new Long(3);
-        Long b = new Long(3);
-        System.out.println(a.longValue()==b.longValue());
-
-
-
-        String regex = "([A-Z0-9a-z()_-]|[\\u4e00-\\u9fa5]|\\s)+";
-        boolean matches = Pattern.matches(regex, "asd()（）反倒是感到十分的广泛的-");
-        System.out.println(matches);
+    public void test1() throws Exception {
 
     }
 }
