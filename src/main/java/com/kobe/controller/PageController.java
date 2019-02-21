8f53a127118fbb9b780850eeb2fa75a27929b3c3
liuqiang147 <liuqiang147@jd.com>
2019-02-21 15:30:52 +0800

    删除无用数据

--- src/main/java/com/kobe/controller/PageController.java
+++ src/main/java/com/kobe/controller/PageController.java
@@ -2,44 +2,30 @@ package com.kobe.controller;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-@Controller
+@RestController
 @Api(description = "页面跳转")
 @RequestMapping
 public class PageController {
 	@GetMapping
-	public ModelAndView jsp() {
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("jsp");
-		return modelAndView;
+	@ApiOperation(value = "首页跳转")
+	public Map jsp() {
+		Map map=new HashMap();
+		map.put("appname","jiashu");
+		map.put("type","小程序");
+		return map;
 	}
 
-	@PostMapping("/uploads")
-	@ApiOperation(value = "上传多个文件")
-	public String uploads(@RequestParam MultipartFile[] files) throws IOException {
-		int length = files.length;
-		if (files != null && files.length > 0) {
-			for (MultipartFile file : files) {
-				file.transferTo(new File("D:\\" + file.getOriginalFilename()));
-			}
-		}
-		return "suc";
-	}
-
-	@PostMapping("/uploadsss")
+	@PostMapping("/fileUpload")
 	@ApiOperation(value = "上传文件")
 	public String upload(@RequestParam MultipartFile file) throws IOException {
-		file.transferTo(new File("D:\\" + file.getOriginalFilename()));
 		return "suc";
 	}
 }
