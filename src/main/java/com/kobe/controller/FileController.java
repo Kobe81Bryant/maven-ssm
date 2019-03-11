1a9fbc05708be908afa7e133ab3e4e703d6e3c8f
liuqiang147 <liuqiang147@jd.com>
2019-03-11 16:36:55 +0800

    改进

--- src/main/java/com/kobe/controller/FileController.java
+++ src/main/java/com/kobe/controller/FileController.java
@@ -4,9 +4,6 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import com.kobe.entity.TbUser;
-import com.kobe.entity.TbUserExample;
-import com.kobe.mapper.TbUserMapper;
 import com.kobe.service.FileService;
 import com.kobe.vo.Response;
 import com.qiniu.common.Zone;
@@ -15,6 +12,7 @@ import com.qiniu.storage.UploadManager;
 import com.qiniu.util.Auth;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.poi.ss.usermodel.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -25,6 +23,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,8 +32,6 @@ import java.util.Map;
 @Api(description = "文件服务")
 @RequestMapping
 public class FileController {
-	@Autowired
-	private TbUserMapper tbUserMapper;
 	@Autowired
 	private FileService fileService;
 	@GetMapping
@@ -54,25 +51,19 @@ public class FileController {
 		return response;
 	}
 
-	@GetMapping("/userList")
-	@ApiOperation(value = "用户列表")
-	public List<TbUser>getUserList(){
-		TbUserExample tbUserExample=new TbUserExample();
-		TbUserExample.Criteria criteria = tbUserExample.createCriteria();
-		List<TbUser> tbUsers = tbUserMapper.selectByExample(tbUserExample);
-		return tbUsers;
-	}
+	@PostMapping("/excelUpload")
+	@ApiOperation(value = "excel上传")
+	public Response getUserList(@RequestParam MultipartFile file) throws Exception{
+		Response response=new Response();
+		InputStream inputStream = file.getInputStream();
+		Workbook workbook = WorkbookFactory.create(inputStream);
+		Sheet sheetAt = workbook.getSheetAt(0);
+		Row row = sheetAt.getRow(1);
+		Cell cell = row.getCell(10);
+		int cellType = cell.getCellType();
+		Date dateCellValue = cell.getDateCellValue();
 
-	@GetMapping("/userListPage")
-	@ApiOperation(value = "用户列表分页")
-	public PageInfo<TbUser>getUserListPage(@RequestParam(defaultValue = "1") Integer page,
-									   @RequestParam(defaultValue = "3") Integer size){
-		PageHelper.startPage(page,size);
-		TbUserExample tbUserExample=new TbUserExample();
-		TbUserExample.Criteria criteria = tbUserExample.createCriteria();
-		List<TbUser> tbUsers = tbUserMapper.selectByExample(tbUserExample);
-		PageInfo<TbUser>pageInfo=new PageInfo<>(tbUsers);
-		return pageInfo;
+		return response;
 	}
 
 }
--- src/test/java/test/TestDemo.java
+++ src/test/java/test/TestDemo.java
@@ -1,12 +1,28 @@
 package test;
 
+import com.kobe.entity.Pojo;
+import com.kobe.service.FileService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/spring/applicationContext-*.xml"})
 public class TestDemo {
+    @Autowired
+    private FileService fileService;
     @Test
-    public void test1() {
-
+    public void test1() throws ParseException {
+        System.out.println(fileService);
+        int i = fileService.hashCode();
+        System.out.println(i);
     }
 }
 
