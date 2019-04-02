0214a0113d6ac80c8bb2499fdfb2b1191c41ca9a
liuqiang147 <liuqiang147@jd.com>
2019-04-02 16:51:59 +0800

    改进

--- src/test/java/test/TestDemo.java
+++ src/test/java/test/TestDemo.java
@@ -1,28 +1,40 @@
 package test;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.kobe.entity.Pojo;
+import com.kobe.entity.SkuNotice;
 import com.kobe.service.FileService;
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/spring/applicationContext-*.xml"})
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"classpath:/spring/applicationContext-*.xml"})
 public class TestDemo {
-    @Autowired
-    private FileService fileService;
+    //    @Autowired
+//    private FileService fileService;
     @Test
     public void test1() throws ParseException {
-        System.out.println(fileService);
-        int i = fileService.hashCode();
-        System.out.println(i);
+        Integer i=5;
+        System.out.println(i.toString());
+
+        String asd="null";
+        List<Integer> list = JSON.parseArray(asd, Integer.class);
+        System.out.println(list);
+        System.out.println(asd);
     }
 }
 
